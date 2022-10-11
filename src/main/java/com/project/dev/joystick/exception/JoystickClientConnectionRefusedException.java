884c83bc28d4b05ff5ad91ec0a58dca0f192e5aa
Dyson <dysontilano@gmail.com>
2022-10-11 11:50:44 -0500

    Updated license of '*.java' files

--- src/main/java/com/project/dev/joystick/exception/JoystickClientConnectionRefusedException.java
+++ src/main/java/com/project/dev/joystick/exception/JoystickClientConnectionRefusedException.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {JoystickClientConnectionRefusedException} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {JoystickClientConnectionRefusedException} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {JoystickClientConnectionRefusedException} fue escrita por Dyson A. Parra T.
  */
 package com.project.dev.joystick.exception;
 
