e1f0e4ac839861085b5cb81ad8176b958526f45d
Dyson <dysontilano@gmail.com>
2023-01-04 15:01:59 -0500

    Fixed License and doc for some files

--- src/main/java/com/project/dev/joystick/button/GenericButtonGraphicListener.java
+++ src/main/java/com/project/dev/joystick/button/GenericButtonGraphicListener.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {GenericButtonGraphicListener<T>} se encarga de realizar tareas especificas.
+ * @fileoverview {GenericButtonGraphicListener} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {GenericButtonGraphicListener<T>} fue realizada el 31/07/2022.
- * @Dev - La primera version de {GenericButtonGraphicListener<T>} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {GenericButtonGraphicListener} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {GenericButtonGraphicListener} fue escrita por Dyson A. Parra T.
  */
 package com.project.dev.joystick.button;
 
--- src/main/java/com/project/dev/joystick/setter/JoystickViewSetter.java
+++ src/main/java/com/project/dev/joystick/setter/JoystickViewSetter.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {JoystickViewSetter<T>} se encarga de realizar tareas especificas.
+ * @fileoverview {JoystickViewSetter} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {JoystickViewSetter<T>} fue realizada el 31/07/2022.
- * @Dev - La primera version de {JoystickViewSetter<T>} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {JoystickViewSetter} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {JoystickViewSetter} fue escrita por Dyson A. Parra T.
  */
 package com.project.dev.joystick.setter;
 
